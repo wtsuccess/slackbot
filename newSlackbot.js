7ab0662fa949127b9efe5e2534f9a7a903187804
trust0212 <zaza.liveedu@gmail.com>
2023-06-20 19:55:20 -0400

    add bolt

--- newSlackbot.js
+++ newSlackbot.js
@@ -11,6 +11,14 @@ app.event('message', (args) => {
   console.log('Event: Message: ', args);
 });
 
+// Reverse all messages the app can hear
+app.message(async ({ message, say }) => {
+  // Filter out message events with subtypes (see https://api.slack.com/events/message)
+  if (message.subtype === undefined || message.subtype === 'bot_message') {
+    const reversedText = [...message.text].reverse().join("");
+    await say(reversedText);
+  }
+});
 
 (async () => {
   // Start the app
