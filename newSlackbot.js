93326762c086d8a1eff694d8768547ab92e0aed4
trust0212 <zaza.liveedu@gmail.com>
2023-06-20 22:24:37 -0400

    add

--- newSlackbot.js
+++ newSlackbot.js
@@ -7,35 +7,28 @@ const app = new App({
 });
 
 /* Add functionality here */
-app.event('message', (args) => {
-  console.log('Event: Message: ', args);
-});
 
-app.event('im', (args) => {
-  console.log('DM Event: ', args)
+app.event('im_created', (args, user) => {
+  console.log('DM Event-Created: ', args, user)
 })
 
-app.event('im_created', (args) => {
-  console.log('DM Event-Created: ', args)
-})
-
-app.event('im_updated', (args) => {
-  console.log('DM Event-Updated: ', args)
+app.event('im_updated', (args, user) => {
+  console.log('DM Event-Updated: ', args, user)
 })
 
 // Reverse all messages the app can hear
-app.message(async ({ message, say }) => {
-  console.log("Message: ", message)
-  // Filter out message events with subtypes (see https://api.slack.com/events/message)
-  if (message.subtype === undefined || message.subtype === 'bot_message') {
-    const reversedText = [...message.text].reverse().join("");
-    await say(reversedText);
-  }
-});
+// app.message(async ({ message, say }) => {
+//   console.log("Message: ", message)
+//   // Filter out message events with subtypes (see https://api.slack.com/events/message)
+//   if (message.subtype === undefined || message.subtype === 'bot_message') {
+//     const reversedText = [...message.text].reverse().join("");
+//     await say(reversedText);
+//   }
+// });
 
-(async () => {
+// (async () => {
   // Start the app
-  await app.start(process.env.PORT || 3000);
+  app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running! - ', process.env.PORT);
-})();
+// })();
